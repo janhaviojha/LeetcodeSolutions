086b4cd2c8331cf92fd32bb6f73c9ae716bce356
janhaviojha <46934900+janhaviojha@users.noreply.github.com>
2021-06-27 19:59:19 +0530

    Update largest-number.java

--- largest-number/largest-number.java
+++ largest-number/largest-number.java
@@ -25,21 +25,7 @@ class Solution {
         }
         
         
-        boolean flag =true;
-        for(int i=0;i<ans.length()-1;i++){
-            if(ans.charAt(i)==ans.charAt(i+1) && ans.charAt(i)=='0' && ans.length()>1){
-                flag=true;
-            }
-            else{
-                flag=false;
-                break;
-            }
-            
-        }
-        if(flag==false)
-            return ans;
-        else
-         return ans.charAt(0)+"";
+        return ans.startsWith("0") ? "0" : ans;
     }
         
         
