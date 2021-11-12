d987ee791a2264b12b91b98a572dd50aa1c2a297
janhaviojha <46934900+janhaviojha@users.noreply.github.com>
2021-11-12 21:36:11 +0530

    Time: 3 ms (67.95%), Space: 40.4 MB (37.54%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,26 +1,31 @@
+import java.util.*;
 class Solution {
-    public int[] twoSum(int[] arr, int target) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        int[] ans=new int[2];
-        int val=0;
-        for(int i=0;i<arr.length;i++){
-            if(hm.containsKey(target-arr[i])){
-                val=target-arr[i];
-                ans[1]=i;
-            }
-            else{
-                 hm.put(arr[i],target-i);
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            hm.put(nums[i],i);
+        }
+        int index1 =0;
+        int index2=0;
+        for(int i=0;i<nums.length;i++){
+            int val = target- nums[i];
+            if(hm.containsKey(val) && hm.get(val)!=i){
+                index1 = i;
+                index2 = hm.get(val);
+                break;
             }
         }
-        // System.out.print(val);
-            for(int i=0;i<arr.length;i++){
-                if(arr[i]==val){
-                    ans[0]=i;
-                    break;
-                }
+               int[] ans = new int[2];
+            if(index1<index2){
+                ans[0]=index1;
+                ans[1]=index2;
             }
-           
-        
-        return ans;
+        else{
+            ans[0]=index2;
+            ans[1]=index1;
+        }
+               
+              
+               return ans;
     }
 }
