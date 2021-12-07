50f3222c71c508475ced974482263a19c5255d07
janhaviojha <46934900+janhaviojha@users.noreply.github.com>
2021-12-07 20:48:07 +0530

    Time: 5 ms (50.40%), Space: 43.9 MB (5.19%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -2,6 +2,7 @@ import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> hm = new HashMap<>();
+        
         for(int i=0;i<nums.length;i++){
             hm.put(nums[i],i);
         }
