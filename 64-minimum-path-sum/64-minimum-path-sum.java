9038fcd1b10f348df2fe13ca1faf1f93eca1c191
janhaviojha <46934900+janhaviojha@users.noreply.github.com>
2022-03-30 20:39:15 +0530

    Time: 2 ms (88.36%), Space: 46.9 MB (8.36%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minPathSum(int[][] cost) {
+        int rows = cost.length;
+        int columns = cost[0].length;
+        //System.out.println(rows +" " + columns);
+        int[][] mincost = new int[rows][columns];
+        mincost[0][0] = cost[0][0];
+        if(columns>1){
+        for(int j=1;j<columns;j++){
+            mincost[0][j] = mincost[0][j-1] + cost[0][j];
+        }
+        }
+        if(rows>1){
+            for(int i=1;i<rows;i++){
+                mincost[i][0] = mincost[i-1][0] + cost[i][0];
+            }
+        }
+
+        for(int i=1;i<rows;i++){
+            for(int j=1;j<columns;j++){
+                mincost[i][j] =  Math.min(mincost[i-1][j],mincost[i][j-1]) + cost[i][j];
+            }
+        }
+        return mincost[rows-1][columns-1];
+    }
+}
