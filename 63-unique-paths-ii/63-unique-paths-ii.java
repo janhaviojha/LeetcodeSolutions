0d0d42f417b38bb39d7730d6567540fa0f63e90f
janhaviojha <46934900+janhaviojha@users.noreply.github.com>
2022-03-31 00:29:52 +0530

    Time: 1 ms (54.47%), Space: 42.5 MB (29.76%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -2,6 +2,7 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int rows = obstacleGrid.length;
         int columns = obstacleGrid[0].length;
+        //making new array because have to initialize obstacles with -1
         int[][] numways = new int[rows][columns];
         for(int i=0;i<rows;i++){
             for(int j=0;j<columns;j++){
@@ -11,26 +12,33 @@ class Solution {
                     numways[i][j] = -1;
             }
         }
+        //initialize first column
         for(int i=0;i<rows;i++){
             if(numways[i][0]!=-1){
                 numways[i][0]=1;
             }
             else break;
         }
+        //initialize first row
          for(int j=0;j<columns;j++){
             if(numways[0][j]!=-1){
                 numways[0][j]=1;
             }
              else break;
         }
+        //if first cell has obtacle e cannot reach end
         if(obstacleGrid[0][0]==-1){
             return 0;
         }
+        
         for(int i=1;i<rows;i++){
             for(int j=1;j<columns;j++){
+                //if cell has obstacle skip it
                 if(numways[i][j]==-1){
                     continue;
                 }
+                //if the previous cell did not have obstacle then add the num of ways to reach                  prev cell  to the num of ways to reach present cell 
+                
                 if(numways[i-1][j]!=-1){
                     numways[i][j] += numways[i-1][j];
                 }
@@ -39,6 +47,7 @@ class Solution {
                 }
             }
         }
+        //if last cell has obstacle then we cannot reach it hence return 0 ways else return             ans
         if(obstacleGrid[rows-1][columns-1]==1){
             return 0;
         }
