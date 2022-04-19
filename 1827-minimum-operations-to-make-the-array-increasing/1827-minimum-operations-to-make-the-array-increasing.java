a8199d474747dd078dc1f9204ca7a61a52aa33f3
janhaviojha <46934900+janhaviojha@users.noreply.github.com>
2022-04-19 15:08:26 +0530

    Time: 4 ms (35.93%), Space: 49.2 MB (39.64%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -1,43 +1,17 @@
 class Solution {
     public int minOperations(int[] nums) {
         int n = nums.length;
-        int max = 0;
-        int maxIndex = 0;
+       
         int count =0;
-        for(int i=0;i<n;i++){
-            if(nums[i]> max){
-                max = nums[i];
-                maxIndex = i;
-            }
-        }
+       
        for(int i=1;i<n;i++){
-           while(nums[i]<=nums[i-1]){
-               nums[i]++;
-               count++;
+           if(nums[i]<=nums[i-1]){
+               count += Math.abs(nums[i]-(nums[i-1]+1));
+                nums[i] = nums[i-1]+1;
            }
-           // while(nums[i]>nums[i-1]){
-           //     nums[i-1]++;
-           //     count++
-           // }
+         
        }
-        // for(int i=0;i<maxIndex;i++){
-        //     int diff = maxIndex - i;
-        //     System.out.print(nums[i]);
-        //     while(nums[i]<max-diff){
-        //         nums[i]++;
-        //         count++;
-        //     }
-                
-        //}
-//         for(int i=maxIndex+1;i<n;i++){
-//             //System.out.print(nums[i]);
-//              int diff = i - maxIndex;
-//             while(nums[i]<max+diff){
-//                 nums[i]++;
-//                 count++;
-//             }
-                
-//         }
+       
         return count;
     }
 }
